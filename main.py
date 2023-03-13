bc4d8a2dc857bb6e9dcde5f2d0fcf11d83642518
Isac Junior <>
2023-03-13 22:50:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+# Projeto simples em python, jogo adivinhação //  visto no canal "Lilizok4 ASMR" # noqa
+
+from random import randint
+
+seu_nome = input('Olá, qual o seu nome? ')
+print(
+    f'Okay! {seu_nome}, tente adivinhar qual numero irei escolher! de 1 à 10.')
+numero_descoberto = randint(1, 10)
+numero_tentativas = 3
+
+for tentativa in range(1, numero_tentativas + 1):
+    numero_chutado = int(input('Escolha um número de 1 à 10: '))
+
+    if numero_chutado == numero_descoberto:
+        print(
+            f'Parabéns !!!, você conseguiu acertar em {tentativa} tentativas!')
+        break
+    elif numero_chutado > numero_descoberto:
+        print(
+            'VOCÊ ERROU!!! tente um número menor!')
+        print(
+            f'Você tem {numero_tentativas - tentativa} tentativas...'
+        )
+    else:
+        print(
+            'VOCÊ ERROU!!! tente um número maior!')
+        print(
+            f'Você tem {numero_tentativas - tentativa} tentativas...'
+        )
+        print(f"Infelizmente você perdeu, o numero era {numero_descoberto}.")
